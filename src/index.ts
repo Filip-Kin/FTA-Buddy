edb4321f7e6bd381be12999005c0607cbbb807fa
Filip Kin <me@filipkin.com>
2024-05-27 01:02:41 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -192,6 +192,7 @@ app.get('/docs/*', async (req, res) => {
     res.send(`
     <html>
         <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="stylesheet" href="/docs.css">
             <link rel="stylesheet" href="/hljs.css">
         </head>
